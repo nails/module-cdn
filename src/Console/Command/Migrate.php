cc0eb81e116f7751e1a276051f08debc182dbc72
Pablo de la Peña <p@shedcollective.org>
2016-03-02 11:24:47 +0000

    Placeholder for CDN migration tool

---
+++ src/Console/Command/Migrate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Nails\Cdn\Console\Command;
+
+use Nails\Console\Command\Base;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Migrate extends Base
+{
+    /**
+     * Configures the app
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName('cdn-migrate');
+        $this->setDescription('Migrates CDN between drivers');
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Executes the app
+     * @param  InputInterface  $input  The Input Interface provided by Symfony
+     * @param  OutputInterface $output The Output Interface provided by Symfony
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('');
+        $output->writeln('<info>--------------------</info>');
+        $output->writeln('<info>CDN Driver Migration</info>');
+        $output->writeln('<info>--------------------</info>');
+        $output->writeln('Beginning...');
+
+        $output->writeln('');
+        $output->writeln('');
+        $output->writeln('<comment>@todo</comment>');
+        $output->writeln('');
+        $output->writeln('');
+
+        //  Cleaning up
+        $output->writeln('');
+        $output->writeln('<comment>Cleaning up...</comment>');
+
+        //  And we're done!
+        $output->writeln('');
+        $output->writeln('Complete!');
+    }
+}
