16761203905e739de7d641d2b0845894573de8f8
Pablo de la Peña <p@shedcollective.org>
2017-12-07 10:26:20 +0000

    Bugfix

--- cdn/views/picker.php
+++ cdn/views/picker.php
@@ -1,6 +1,6 @@
 <div class="cdn-object-picker" data-bucket="<?=$sBucket?>" <?=$sAttr?>>
     <input class="cdn-object-picker__input" type="hidden" name="<?=$sKey?>" value="<?=$iObjectId?>" <?=$sInputAttr?>>
-    <button type="button" class="cdn-object-picker__btn btn btn-sm btn-primary" <?$bReadOnly ? 'disabled' : ''?>>
+    <button type="button" class="cdn-object-picker__btn btn btn-sm btn-primary" <?=$bReadOnly ? 'disabled' : ''?>>
         Browse
     </button>
     <b class="cdn-object-picker__pending fa fa-spinner fa-spin"></b>
