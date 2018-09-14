e3eee5bc5bfdf50b2e20938171f057fa4b974d51
Pablo de la Peña <p@shedcollective.org>
2018-09-14 13:54:30 +0100

    Added migration to rename `nailsapp` items in nails_cdn_object and nails_cdn_object_trash to `nails`

---
+++ migrations/9.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Migration:   9
+ * Started:     14/09/2018
+ * Finalised:   14/09/2018
+ *
+ * @package     Nails
+ * @subpackage  module-cdn
+ * @category    Database Migration
+ * @author      Nails Dev Team
+ */
+
+namespace Nails\Database\Migration\Nails\ModuleCdn;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration9 extends Base
+{
+    /**
+     * Execute the migration
+     * @return void
+     */
+    public function execute()
+    {
+        $this->query("UPDATE `{{NAILS_DB_PREFIX}}cdn_object` SET `driver` = REPLACE(`driver`, 'nailsapp', 'nails');");
+        $this->query("UPDATE `{{NAILS_DB_PREFIX}}cdn_object_trash` SET `driver` = REPLACE(`driver`, 'nailsapp', 'nails');");
+    }
+}
