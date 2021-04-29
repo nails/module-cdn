af2de0ae5c329e9c9a6ce0f2d73d33d52b3186d9
Pablo de la Peña <p@shedcollective.org>
2021-04-29 11:22:06 +0100

    Add properties to Bucket resource

--- src/Resource/Bucket.php
+++ src/Resource/Bucket.php
@@ -11,4 +11,21 @@ use Nails\Common\Resource\Entity;
  */
 class Bucket extends Entity
 {
+    /** @var string */
+    public $slug;
+
+    /** @var string */
+    public $label;
+
+    /** @var string */
+    public $allowed_types;
+
+    /** @var int */
+    public $max_size;
+
+    /** @var int */
+    public $disk_quota;
+
+    /** @var bool */
+    public $is_hidden;
 }
