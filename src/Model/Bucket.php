2ea3df4fcc93630e220bd7b16cd93b047e2c6017
Pablo de la Peña <p@shedcollective.org>
2021-04-29 11:22:22 +0100

    Simplify Bucket model

--- src/Model/Bucket.php
+++ src/Model/Bucket.php
@@ -13,6 +13,7 @@
 namespace Nails\Cdn\Model;
 
 use Nails\Cdn\Constants;
+use Nails\Common\Exception\ModelException;
 use Nails\Common\Model\Base;
 use Nails\Config;
 
@@ -33,41 +34,12 @@ class Bucket extends Base
 
     /**
      * Bucket constructor.
+     *
+     * @throws ModelException
      */
     public function __construct()
     {
         parent::__construct();
-        $this
-            ->addExpandableField([
-                'trigger'   => 'objects',
-                'type'      => self::EXPANDABLE_TYPE_MANY,
-                'property'  => 'objects',
-                'model'     => 'Object',
-                'provider'  => Constants::MODULE_SLUG,
-                'id_column' => 'bucket_id',
-            ]);
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Formats a single object
-     *
-     * @param object $oObj      A reference to the object being formatted.
-     * @param array  $aData     The same data array which is passed to _getCountCommon, for reference if needed
-     * @param array  $aIntegers Fields which should be cast as integers if numerical and not null
-     * @param array  $aBools    Fields which should be cast as booleans if not null
-     * @param array  $aFloats   Fields which should be cast as floats if not null
-     */
-    protected function formatObject(
-        &$oObj,
-        array $aData = [],
-        array $aIntegers = [],
-        array $aBools = [],
-        array $aFloats = []
-    ) {
-        $aIntegers[] = 'max_size';
-        $aIntegers[] = 'disk_quota';
-        parent::formatObject($oObj, $aData, $aIntegers, $aBools, $aFloats);
+        $this->hasMany('objects', 'Object', 'bucket_id', Constants::MODULE_SLUG);
     }
 }
