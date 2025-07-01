7e997df00728bf27fa296b579fdcd4b0214234f8
Pablo de la Peña <p@shedcollective.org>
2025-07-01 09:49:09 +0100

    chore: Fix bug; missing declaration

--- src/Service/Cdn.php
+++ src/Service/Cdn.php
@@ -1326,6 +1326,7 @@ class Cdn
         switch ($iErrorNumber) {
 
             case UPLOAD_ERR_INI_SIZE:
+                $sMax = static::maxUploadSize();
                 return sprintf(
                     'The file exceeds the maximum size accepted by this server%s.',
                     $sMax ? '(which is ' . $sMax . ')' : ''
